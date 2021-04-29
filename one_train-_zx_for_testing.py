dce4a493a25076ad510d1e1e2baadfc896c0bed6
kallenxuzy <kallen.xuzy@outlook.com>
2021-04-29 18:01:35 -0400

    pred_rdd one train

--- one_train-_zx_for_testing.py
+++ one_train-_zx_for_testing.py
@@ -44,17 +44,17 @@ def main(spark, train_path, val_path, indexer_model):
 
     rec_result = model.recommendForUserSubset(user_id,500)
     pred_tracks = rec_result.select('user_idx','recommendations.track_idx')\
-                .withColumnRenamed('recommendations.track_idx', 'tracks').sort('user_idx')
+                .withColumnRenamed('recommendations.track_idx', 'tracks')
 
-    true_tracks = val.select('user_idx', 'track_idx')\
-                    .groupBy('user_idx')\
-                    .agg(expr('collect_list(track_idx) as true_item'))
-    #w = Window.partitionBy('user_idx').orderBy(col('count').desc())
-    #true_tracks = val.select('user_idx', 'track_idx', 'count', F.rank().over(w).alias('rank')) \
-                    #.where('rank <= {0}'.format(500)).groupBy('user_idx') \
-                    #.agg(expr('collect_list(track_idx) as tracks'))
+    #true_tracks = val.select('user_idx', 'track_idx')\
+    #                .groupBy('user_idx')\
+    #                .agg(expr('collect_list(track_idx) as true_item'))
+    w = Window.partitionBy('user_idx').orderBy(col('count').desc())
+    true_tracks = val.select('user_idx', 'track_idx', 'count', F.rank().over(w).alias('rank')) \
+                    .where('rank <= {0}'.format(500)).groupBy('user_idx') \
+                    .agg(expr('collect_list(track_idx) as true_track_idx'))
     #true_tracks = true_tracks.select('user_idx', '')
-    pred_RDD = pred_tracks.join(true_tracks, 'user_idx', 'inner').rdd.map(lambda row: (row[1], row[2]))
+    pred_RDD = pred_tracks.join(true_tracks, 'user_idx').rdd.map(lambda row: (row['tracks'], row['true_track_idx']))
     ranking_metrics = RankingMetrics(pred_RDD)
     map_ = ranking_metrics.meanAveragePrecision
     mpa = ranking_metrics.precisionAt(500)
