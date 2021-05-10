fe56c166c6b332ccbfb5877c5e6830b8e0b88543
kallenxuzy <kallen.xuzy@outlook.com>
2021-05-09 21:56:16 -0400

    try first size

--- als_model_extension_2.py
+++ als_model_extension_2.py
@@ -35,7 +35,7 @@ def main(spark, train_path, val_path, test_path):
     precs=[]
 
     for numrow in [49923]:#, 248838, 497832, 2492065, 4982921, 7471981, 9963586, 17435567]: # of rows - 0.1%, 0.5%, 1%, 5%, 10%, 15%, 20%, 30% of all records - from light
-        train_sampled = random.sample(train, numrow)
+        train_sampled = random.sample(list(train), numrow)
 
         indexer_user = StringIndexer(inputCol="user_id", outputCol="user_idx",handleInvalid='skip')
         indexer_track = StringIndexer(inputCol="track_id", outputCol="track_idx",handleInvalid='skip')
